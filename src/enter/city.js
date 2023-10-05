b5af97c2a2efe738fb47fa7977c01887b2d24877
星霜下逆天生 <1486252015@qq.com>
2023-10-05 16:52:05 +0800

    17.12特效：透明墙

--- src/enter/city.js
+++ src/enter/city.js
@@ -4,7 +4,7 @@ import * as TWEEN from "@tweenjs/tween.js";
 import { SurroundLine } from "../effect/surroundLine";
 import { Background } from "../effect/background";
 import { Radar } from "../effect/radar";
-
+import { Wall } from "../effect/wall";
 export class City {
   constructor(scene, camera) {
     console.log(scene);
@@ -37,6 +37,7 @@ export class City {
     new Background(this.scene);
 
     new Radar(this.scene, this.time);
+    new Wall(this.scene, this.time);
 
     //添加点击选择
     this.addClick();
